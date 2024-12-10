9f52d65037922a31f6a1ed57d942cf5802ac49e6
Mike White <157448906+mikewhitecodes@users.noreply.github.com>
2024-12-10 18:11:25 -0500

    Add files via upload
    inputing variables and values into the console to learn initial javascript

---
+++ learning/index.js
@@ -0,0 +1,43 @@
+// // document.getElementById("count-el").innerText=5
+
+
+
+// let count=0
+
+// console.log(count)
+
+// // 1. Create a variable, myAge, and set its value to your age
+// let myAge=32
+
+// // 2. Log the myAge variable to the console
+// console.log(myAge)
+
+
+// // create 2 variables, myAge and humanDogRatio
+// // let myAge=32
+// let humanDogRatio=7
+
+// // multiply the two together and store the result in myDogAge
+// let myDogAge=myAge*humanDogRatio
+
+
+// // log myDogAge to the console
+// console.log(myDogAge)
+
+
+// initialize the count as 0
+
+
+ //listen for clicks on the increment button
+
+
+ // increment the count variable when the button is clicked
+
+
+ // change the count-el in the HTML to reflect the new count
+
+
+
+ function increment() {
+  console.log("The button was clicked")
+ }
